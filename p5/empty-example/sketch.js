06ce47338cf84728f47b3209d7752db11d93185e
David Whipple <dwhipp88@gmail.com>
2022-08-22 21:12:18 -0600

    initial commit

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  // console.log('setup');
+}
+
+function draw() {
+  background(155, 200, 200);
+  rect(50, 100, 100, 100);
+  rect(200, 100, 100, 100);
+  ellipse(175, 300, 150, 75);
+  line(50, 100, 150, 200);
+  line(200, 200, 300, 100);
+  line(250, 300, 100, 300)
+}
