30967994193bbca0de1853a7e0f77dfd84f8b102
David Whipple <dwhipp88@gmail.com>
2022-08-25 19:58:56 -0600

    learned about simple animations

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,25 +1,30 @@
+let count = 0;
+
 function setup() {
-  createCanvas(400, 400);
-  // console.log('setup');
+  let canvasWidth = 800;
+  let canvasHeight = 400;
+
+  createCanvas(canvasWidth, canvasHeight);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
-  // circle one
-  fill(255, 0, 0);
-  ellipse(200, 200, 100, 100);
-  fill(111, 211, 1);
-  ellipse(200, 200, 90, 90);
-  // circle two
-  fill(0, 244, 0);
-  ellipse(100, 100, 100, 100);
-  fill(111, 1, 211);
-  ellipse(100, 100, 90, 90);
-  // circle three
-  fill(0, 0, 255);
-  ellipse(300, 300, 100, 100);
-  fill(1, 211, 111);
-  // noFill();
-  stroke(0);
-  ellipse(300, 300, 90, 90);
+  let canvasWidth = 800;
+  let canvasHeight = 400;
+
+  background(1, 185, 244);
+  let x = count / 2;
+  let y = canvasHeight / 2;
+  let size = 200 + count;
+
+  // circle
+  fill(237, 34, 93);
+  noStroke();
+  ellipse(x, y, size, size);
+
+  // rectangle
+  fill(225);
+  rect(x, y, 150, 30);
+
+  count = count + 1;
 }
