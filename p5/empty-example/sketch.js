c7677b5cb89bff3e67b7d7af2ef6abefcab5815c
David Whipple <dwhipp88@gmail.com>
2022-08-30 20:59:35 -0600

    explored constructor functions with a visual aid

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,23 +1,39 @@
-let count = 0;
-
-// let toggle = true;
+let circle;
+let circle_1;
+let circle_2;
 
 function setup() {
   createCanvas(800, 300);
+  circle = new Circle();
+  circle_1 = new Circle();
+  circle_2 = new Circle();
 }
 
 function draw() {
   background(0, 0, 0);
-  let x = squared(64);
-  console.log(x);
+
+  circle.draw();
+  circle.grow();
+  circle_1.x = 150;
+  circle_1.draw();
+  circle_1.grow();
+  circle_2.x = 550;
+  circle_2.draw();
+  circle_2.grow();
 }
 
-function squared(num) {
-  let result = num * num;
-  fill(234, 200, 23);
-  textSize(60);
-  textAlign(CENTER, CENTER);
-  text(result, width / 2, height / 2);
+function Circle() {
+  this.x = width / 2;
+  this.y = height / 2;
+  this.size = 50;
 
-  return result;
+  this.draw = function () {
+    ellipse(this.x, this.y, this.size, this.size);
+  };
+
+  this.grow = function () {
+    if (this.size < 200) {
+      this.size = this.size + 1;
+    }
+  };
 }
