af8b83b7db9938ed573d2ffdad462ae282f6a662
David Whipple <dwhipp88@gmail.com>
2022-10-27 16:41:55 -0600

    iterated through array with forloop

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,39 +1,16 @@
-let circle;
-let circle_1;
-let circle_2;
+let sizes = [200, 150, 100, 50, 25];
 
 function setup() {
   createCanvas(800, 300);
-  circle = new Circle();
-  circle_1 = new Circle();
-  circle_2 = new Circle();
 }
 
 function draw() {
-  background(0, 0, 0);
+  background(220);
 
-  circle.draw();
-  circle.grow();
-  circle_1.x = 150;
-  circle_1.draw();
-  circle_1.grow();
-  circle_2.x = 550;
-  circle_2.draw();
-  circle_2.grow();
-}
+  fill(237, 231, 4);
+  strokeWeight(2);
 
-function Circle() {
-  this.x = width / 2;
-  this.y = height / 2;
-  this.size = 50;
-
-  this.draw = function () {
-    ellipse(this.x, this.y, this.size, this.size);
-  };
-
-  this.grow = function () {
-    if (this.size < 200) {
-      this.size = this.size + 1;
-    }
-  };
+  for (let i = 0; i < 5; i++) {
+    ellipse(width / 2, height / 2, sizes[i], sizes[i]);
+  }
 }
