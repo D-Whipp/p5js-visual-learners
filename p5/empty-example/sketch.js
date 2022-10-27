dee990df1d4c082038f40a519f99c35773c2afb6
David Whipple <dwhipp88@gmail.com>
2022-10-27 17:32:49 -0600

    printed text to screen

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,16 +1,26 @@
-let sizes = [200, 150, 100, 50, 25];
+let words = ['I', 'love', 'programming', 'with', 'JavaScript'];
+
+let sizes = [];
 
 function setup() {
   createCanvas(800, 300);
+  for (let i = 0; i < 100; i++) {
+    let randomValue = random(5, 500);
+    sizes.push(randomValue);
+  }
+  textAlign(CENTER, CENTER)
 }
 
 function draw() {
-  background(220);
+  background(200);
 
-  fill(237, 231, 4);
-  strokeWeight(2);
-
-  for (let i = 0; i < 5; i++) {
-    ellipse(width / 2, height / 2, sizes[i], sizes[i]);
-  }
+  
+  // noFill();
+  // strokeWeight(2);
+  // for (let i = 0; i < sizes.length; i++) {
+  //   ellipse(width / 2, height / 2, sizes[i], sizes[i]);
+  // }
+  textSize(45)
+  fill(255);
+  text('JavaScript', width / 2, height / 2)
 }
